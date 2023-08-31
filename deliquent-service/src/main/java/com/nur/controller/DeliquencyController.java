d262546df6d740076a08c2f723c44cb7be626aae
Nurzamal <n.zaman98@gmail.com>
2023-09-01 00:27:59 +0530

    Delinquency Calculation modification

--- deliquent-service/src/main/java/com/nur/controller/DeliquencyController.java
+++ deliquent-service/src/main/java/com/nur/controller/DeliquencyController.java
@@ -9,7 +9,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -19,11 +18,28 @@ public class DeliquencyController {
     @Autowired
     private DelinquencyCalcService delinquencyCalcService;
 
+/*
+    private DelinquencyMonthRecord mapToFormattedMonthlyRecords(DelinquencyMonthRecord record) {
+        DelinquencyMonthRecord monthRecord = new DelinquencyMonthRecord();
+        monthRecord.setPrincipleBalance(record.getPrincipleBalance());
+        monthRecord.setPrincipleAndInterest(record.getPrincipleAndInterest());
+
+        // Format the interest rate as a percentage with the '%' symbol
+        double interestRatePercentage = record.getInterestRate() * 100;
+        monthRecord.setInterestRate(interestRatePercentage + "%");
+
+        monthRecord.setDueDate(record.getDueDate());
+        monthRecord.setInterest(record.getInterest());
+        monthRecord.setPrinciple(record.getPrinciple());
+        return monthRecord;
+    }
+*/
+
     @PostMapping(value = "/api/steprate/delinquency/v1/calculate", produces = {"application/json"}, consumes = {"application/json"})
     public ResponseEntity<List<DelinquencyMonthRecord>> delinquencyCalculation(@RequestBody DelinquencyRequest delinquencyRequest){
         List<DelinquencyMonthRecord> delinquencyMonthlyRecords = delinquencyCalcService.getDelinquencyMonthlyRecords(delinquencyRequest);
-        List<DelinquencyMonthRecord> delinquencyMonthRecordList = delinquencyMonthlyRecords.stream().map(record -> mapToFormattedMonthlyRecords(record)).collect(Collectors.toList());
-//        List<DelinquencyMonthRecord> delinquencyMonthRecordList = delinquencyMonthlyRecords.stream().map(this:: mapToFormattedMonthlyRecords).collect(Collectors.toList());
+//        List<DelinquencyMonthRecord> delinquencyMonthRecordList = delinquencyMonthlyRecords.stream().map(record -> mapToFormattedMonthlyRecords(record)).collect(Collectors.toList());
+        List<DelinquencyMonthRecord> delinquencyMonthRecordList = delinquencyMonthlyRecords.stream().map(this:: mapToFormattedMonthlyRecords).collect(Collectors.toList());
         return new ResponseEntity<>(delinquencyMonthRecordList, HttpStatus.OK);
     }
 
@@ -37,6 +53,5 @@ public class DeliquencyController {
         monthRecord.setPrinciple(record.getPrinciple());
         return monthRecord;
     }
-
 
 }
